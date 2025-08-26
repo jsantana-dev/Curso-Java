5067d6f396c078cbc746086b14770438582817bb
Jamylle Santana <milysnap@gmail.com>
2025-08-26 15:47:26 -0300

    Refatoração

--- PrimeiroPrograma.java/src/Classe/Produto.java
+++ PrimeiroPrograma.java/src/Classe/Produto.java
@@ -5,4 +5,10 @@ public class Produto {
     String nome;
     double preco;
     double desconto;
+
+    //pode ser usado também os parâmetros que ficam dentro dos ()
+    //mas só ficaria acessível ao chamar o metodo precoComDesconto
+    double precoComDesconto() {
+        return preco * (1 - desconto);
+    }
 }
