4f18e7d0f8b14a5b377763415512a4ac9716a7b6
Jamylle Santana <milysnap@gmail.com>
2025-08-28 16:51:57 -0300

    Membros de Classe vs Instância #01

---
+++ PrimeiroPrograma.java/src/Classe/AreaCirc.java
@@ -0,0 +1,15 @@
+package Classe;
+
+public class AreaCirc {
+
+    double raio;
+    double pi;
+
+    AreaCirc(double raioInicial) {
+        pi = 3.14;
+        raio = raioInicial;
+    }
+    double area(){
+        return pi * Math.pow(raio, 2);
+    }
+}
