88c20df6ff1a8b669b3b146f6a95d61e9da07822
Jamylle Santana <milysnap@gmail.com>
2025-09-09 15:47:04 -0300

    Membros de Classe vs Instância #02

--- PrimeiroPrograma.java/src/Classe/AreaCircTeste.java
+++ PrimeiroPrograma.java/src/Classe/AreaCircTeste.java
@@ -4,11 +4,23 @@ public class AreaCircTeste {
     public static void main(String[] args) {
 
         AreaCirc a1 = new AreaCirc(10); //métodos de instância (objeto)
-        a1.pi = 10;
+        //a1.pi = 10;
         System.out.println(a1.area());
 
         AreaCirc a2 = new AreaCirc(5);
-        a2.pi = 5;
+        //a2.pi = 5;
         System.out.println(a2.area());
+
+        //AreaCirc.PI = 3.1415;
+        System.out.println(AreaCirc.PI);
+        System.out.println(Math.PI);
     }
 }
+
+/* desafios membros de classe vs instância
+
+ alterar a classe do produto que já foi criada
+ quando um desconto é aplicado, ele vale para todos os produtos sem exceção
+ desconto ser uma variável de classe
+ desconto pode variar mas por padrão é 25%
+ */
