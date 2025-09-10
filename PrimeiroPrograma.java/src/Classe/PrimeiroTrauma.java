cb0450cf41fde2b5933967678ca620493978476d
Jamylle Santana <milysnap@gmail.com>
2025-09-10 20:14:56 -0300

    Desafio Primeiro Trauma

---
+++ PrimeiroPrograma.java/src/Classe/PrimeiroTrauma.java
@@ -0,0 +1,15 @@
+package Classe;
+
+public class PrimeiroTrauma {
+
+    int a = 3; //não pode mexer aqui!
+
+    public static void main(String[] args) {
+
+        System.out.println(a);
+    }
+}
+
+/*
+desafio: corrigir o erro, mexer apenas no bloco de código main
+ */
